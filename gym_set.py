ac7cc683b09f0df3366c4624b9e9db1799fc09e5
sumit3645 <154312428+sumit3645@users.noreply.github.com>
2024-10-17 02:19:09 +0530

    Update gym_set.py

--- gym_set.py
+++ gym_set.py
@@ -1,6 +1,14 @@
+#for vertical set up count
 for i in range(1,3):
   print(f"{i} set")
   for j in range(1,11):
     print(j)
+  print()  
+  
+#for horizontal set up count
+for i in range(1,3):
+  print(f"{i} set")
+  for j in range(1,11):
+    print(j , end=" ")
   print()  
   
